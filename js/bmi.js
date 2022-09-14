b463bd7c24ce3eb6b2261d85dda808537c833a49
Alex <alexzalox@gmail.com>
2022-09-14 18:50:09 +0800

    完成評論

--- js/bmi.js
+++ js/bmi.js
@@ -1,6 +1,5 @@
 const getBMI = (height, weight, point = 2) => (weight / (height / 100) ** 2).toFixed(point);
 // tag,class,id
-
 const elementEl = document.querySelector(".title");
 const calcEl = document.querySelector("#calc");
 const heightEl = document.querySelector("#height");
@@ -10,13 +9,14 @@ console.log(elementEl);
 console.log(calcEl);
 
 elementEl.innerHTML = "<u>BMI Calculator</u>";
-calcEl.innerText = "分析"
+calcEl.innerText = "分析";
 
 // 將按鈕增加事件監聽
 calcEl.addEventListener("click", () => {
 
 
     let bmiEl = document.querySelector("#comment .bmi");
+    let textEl = document.querySelector("#comment .text");
     console.log(bmiEl);
 
     let height = heightEl.value;
@@ -24,6 +24,7 @@ calcEl.addEventListener("click", () => {
     if (height == "" || weight == "") {
         alert("輸入不能為空!");
         return;
+
     }
     let bmi = getBMI(height, weight);
     if (isNaN(bmi)) {
@@ -31,22 +32,25 @@ calcEl.addEventListener("click", () => {
         return;
     }
 
+    console.log(bmi);
+    bmiEl.innerHTML = ":" + bmi;
 
     let comments = [
+        { 'bmi': 45, 'color': 'red', 'comment': '你真的太胖了，請不要再吃了!' },
         { 'bmi': 35, 'color': 'red', 'comment': '異常範圍，請節食運動!' },
         { 'bmi': 24, 'color': 'green', 'comment': '正常範圍，恭喜你，請繼續保持。' },
         { 'bmi': 18.5, 'color': 'blue', 'comment': '體重過輕，請多攝取營養。' },
     ];
 
-    bmiEl.style.color = "blue";
+    bmiEl.style.color = comments[comments.length - 1]["color"];
+    textEl.innerText = comments[comments.length - 1]["comment"];
+
     for (let i = 0; i < comments.length; i++) {
         if (bmi >= comments[i].bmi) {
             bmiEl.style.color = comments[i]["color"];
+            textEl.innerText = comments[i]["comment"];
             break;
         }
     }
 
-
-    console.log(bmi);
-    bmiEl.innerHTML = bmi;
 });
